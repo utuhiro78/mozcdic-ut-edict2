d1ed1947139477116c654103767909b04ee0bcd6
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-13 01:54:16 +0900

    Convert to Python

--- src/make.sh
+++ src/make.sh
@@ -3,8 +3,8 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
-ruby convert_edict2_to_mozcdic.rb
-ruby adjust_entries.rb mozcdic-ut-edict2.txt
+python convert_edict2_to_mozcdic.py
+python adjust_entries.py mozcdic-ut-edict2.txt
 
 tar cjf mozcdic-ut-edict2.txt.tar.bz2 mozcdic-ut-edict2.txt
 mv mozcdic-ut-edict2.txt* ../
